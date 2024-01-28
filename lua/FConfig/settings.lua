91274d4d1d44c2389172a517aabd9479ddefc001
Fozzyack <fsundra@gmail.com>
2024-01-29 01:34:53 +0800

    adding a bit more

--- lua/FConfig/settings.lua
+++ lua/FConfig/settings.lua
@@ -3,8 +3,8 @@ vim.opt.termguicolors = true
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.smartindent = true
-
 vim.opt.guicursor = ''
+
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
@@ -13,6 +13,6 @@ vim.opt.expandtab = true
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
-vim.opt.wrap = false
+vim.opt.wrap = true
 vim.opt.scrolloff = 10
 vim.opt.updatetime = 50
