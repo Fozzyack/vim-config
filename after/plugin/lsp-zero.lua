42d872765fc1d04f3673e8e18f3fcc103bf71bf5
Frasier <fsundra@gmail.com>
2024-02-02 02:39:24 +0800

    changing lsp binds

--- after/plugin/lsp-zero.lua
+++ after/plugin/lsp-zero.lua
@@ -1,5 +1,14 @@
 local lsp_zero = require('lsp-zero')
+local cmp = require('cmp')
+local cmp_action = lsp_zero.cmp_action()
 
+cmp.setup({
+    mapping = {
+        ['<C-k>'] = cmp.mapping.select_prev_item(),
+        ['<C-j>'] = cmp.mapping.select_next_item(),
+        ['<C-u>'] = cmp_action.toggle_completion(),
+    }
+})
 lsp_zero.on_attach(function(client, bufnr)
 	--	see :help lsp-zero-keybindings
 	-- to learn the available actions
