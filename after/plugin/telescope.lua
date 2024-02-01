7516f9f00cb6adaa343b46e8c7a5dd3f81f2168b
Frasier <fsundra@gmail.com>
2024-02-01 14:21:50 +0800

    Changing telescope config to f and g

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,3 +1,3 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<leader>pg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>f', builtin.find_files, {})
+vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
