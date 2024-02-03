b1613b51386eed62d96f889b743393ac0e78e01c
Frasier <fsundra@gmail.com>
2024-02-03 13:57:53 +0800

    Adding a Yanking to system clipboard into vim Make sure the clipboard is set Run :checkhealth and search for clipboard to see if it is working Should be able to yank into register +y

--- lua/FConfig/maps.lua
+++ lua/FConfig/maps.lua
@@ -4,3 +4,4 @@ vim.g.mapleader = ' '
 vim.keymap.set('n', '<leader>q', vim.cmd.Ex)
 vim.keymap.set('n', '<C-d>', '<C-d>zz')
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('v', '<leader>y', '"+y') 
